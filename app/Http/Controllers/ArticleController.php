2450648382ae1706362ef93343f68fcc8143eb27
Aziz <alex.mavlyanov95@gmail.com>
2021-02-22 14:31:45 +0300

    Add tests for ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Support\Facades\Cache;
 
 
 class ArticleController extends Controller
 {   
     public function index(Request $request)
     {   
+        $value = Cache::get('spy_index_call');
+
         // It returns list of articles with embedded photo and user data
         $user_id = $request->query('user_id');
 
@@ -21,19 +24,23 @@ class ArticleController extends Controller
     }
 
     public function show($id)
-    {
+    {   
+        $value = Cache::get('spy_show_call');
+
         return Article::with(['photo', 'user'])->get()->find($id);
     }
 
     public function store(Request $request)
-    {
+    {   
+        $value = Cache::get('spy_store_call');
+
         $article = Article::create($request->all());
 
         return response()->json($article, 201);
     }
 
     public function update(Request $request, Article $article)
-    {
+    {   
         $article->update($request->all());
 
         return response()->json($article, 200);
--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -2,12 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Mockery;
+use Mockery\MockInterface;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ArticleTest extends TestCase
-{
+{   
+    use WithoutMiddleware;
     /**
      * A basic feature test example.
      *
@@ -18,6 +24,34 @@ class ArticleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+
+    public function test_articles_controller_index_call()
+    {   
+        Cache::shouldReceive('get')
+                    ->once()
+                    ->with('spy_index_call');
+
+        $response = $this->get('/api/articles');
+    }
+
+    public function test_articles_controller_show_call()
+    {   
+        Cache::shouldReceive('get')
+                    ->once()
+                    ->with('spy_show_call');
+
+        $response = $this->get('/api/articles/12');
+    }
+
+    public function test_articles_controller_store_call()
+    {   
+        Cache::shouldReceive('get')
+                    ->once()
+                    ->with('spy_store_call');
+
+        $response = $this->post('/api/articles', []);
     }
 
 }
